3216b919301a348e35d1dd2ab0e397f250b46ad1
John-Paul Molden <john.paul.molden@gmail.com>
2018-10-12 00:59:59 -0700

    VirtualEnv

--- section2_MultilineStatementsStrings.py
+++ section2_MultilineStatementsStrings.py
@@ -1,4 +1,32 @@
 import datetime
+# from builtins import dict
 
-print("Hello World")
-print(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
+def my_func(a,
+            b,
+            c):
+    print(a,b,c)
+
+
+print(datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"))
+# List with multiline comments
+a = [1,2,3]
+
+a = [1,  #comment
+    2,
+    3]
+
+
+for i in a:
+    print(i)
+# Tuples
+tup = (1,2,3,3)
+
+print("Tuple: ", tup)
+# Dict
+
+dict = {'key1': 1,
+        'key2': 2}
+print("Dictionary", dict)
+
+# Call Function
+my_func("F",2,3)
