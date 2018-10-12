e462a4c28d03983a8a1b59c76427ed754399076d
John-Paul Molden <john.paul.molden@gmail.com>
2018-10-11 23:10:50 -0700

    Added scripts

---
+++ .scripts/init.sh
@@ -0,0 +1,6 @@
+echo "# python3-deep-dive-udemy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/jpmolden/python3-deep-dive-udemy.git
+git push -u origin master
