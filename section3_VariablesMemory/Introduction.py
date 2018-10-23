a78103ab6e957094a46fcb7100de8b1aac5c8f21
John-Paul Molden <john.paul.molden@gmail.com>
2018-10-23 01:05:03 -0700

    Functions

---
+++ section3_VariablesMemory/Introduction.py
@@ -0,0 +1,19 @@
+
+# Variables in the context of memory references
+
+    # memory references
+    # reference counting
+        # garbage collection
+    # dynamic vs static typing
+    # mutability vs immutability
+    # shared references - same object area in memory
+    # Variable equality
+
+    # EVERYTHING IS AN OBJECT!!!
+    # FUNCTIONS ARE OBJECTS, CLASSES ARE OBJECTS,
+    # STRINGS ARE OBJECTS...
+    #
+    #
+
+
+
---
+++ section3_VariablesMemory/VariablesAreMemoryReferences.py
@@ -0,0 +1,9 @@
+
+# The heap contains dynamically created objects
+
+# Creates an alias (reference) for the memory address where the ocj is stored
+my_val_1 = 10
+my_val_2 = 123564564564564
+
+#
+print(hex(id(my_val_1)))
